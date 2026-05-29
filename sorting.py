6877cf5f3b65266d2c34ee35fa2cb66d1e8fca29
AmdSidiq <ahmadsidiq796@gmail.com>
2026-05-30 00:44:50 +0700

    Fungsi sorting harga

--- sorting.py
+++ sorting.py
@@ -8,3 +8,30 @@ def quick_sort_rating(games):
     right = [x for x in games[1:] if x.rating > pivot.rating]
 
     return quick_sort_rating(right) + [pivot] + quick_sort_rating(left)
+
+def merge_sort_price(games):
+    if len(games) <= 1:
+        return games
+
+    mid = len(games) // 2
+    left = merge_sort_price(games[:mid])
+    right = merge_sort_price(games[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i].price < right[j].price:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
