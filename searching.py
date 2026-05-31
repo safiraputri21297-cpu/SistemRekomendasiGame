d3cdf4d2c5a7ba77681b7e3a04cd827c5cd6bba3
AmdSidiq <ahmadsidiq796@gmail.com>
2026-05-31 15:30:37 +0700

    Fungsi binary search

--- searching.py
+++ searching.py
@@ -4,3 +4,21 @@ def linear_search(games, target):
             return game
 
     return None
+
+def binary_search(games, target):
+    low = 0
+    high = len(games) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if games[mid].judul.lower() == target.lower():
+            return games[mid]
+
+        elif games[mid].judul.lower() < target.lower():
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return None
