08427e2f6a32c9e956ead89733ada4e9cf0ab8e9
AmdSidiq <ahmadsidiq796@gmail.com>
2026-05-31 17:44:20 +0700

    Buat class wishlist

--- linked_list.py
+++ linked_list.py
@@ -2,3 +2,31 @@ class Node:
     def __init__(self, game):
         self.game = game
         self.next = None
+
+class Wishlist:
+    def __init__(self):
+        self.head = None
+
+    def add_game(self, game):
+        new_node = Node(game)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    def display(self):
+        games = []
+        temp = self.head
+
+        while temp:
+            games.append(temp.game.judul)
+            temp = temp.next
+
+        return games
