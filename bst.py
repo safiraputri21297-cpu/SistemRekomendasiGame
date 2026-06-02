7164f6a1569ee26cb2671fb058bdd9eaa9bb7e95
safiraputri21297 <safiraputri21297@gmail.com>
2026-06-02 11:23:21 +0700

    Buat class binary search tree

--- bst.py
+++ bst.py
@@ -3,3 +3,41 @@ class BSTNode:
         self.game = game
         self.left = None
         self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, game):
+        if self.root is None:
+            self.root = BSTNode(game)
+        else:
+            self._insert(self.root, game)
+
+    def _insert(self, node, game):
+        if game.title.lower() < node.game.title.lower():
+            if node.left is None:
+                node.left = BSTNode(game)
+            else:
+                self._insert(node.left, game)
+
+        else:
+            if node.right is None:
+                node.right = BSTNode(game)
+            else:
+                self._insert(node.right, game)
+
+    def search(self, title):
+        return self._search(self.root, title)
+
+    def _search(self, node, title):
+        if node is None:
+            return None
+
+        if node.game.title.lower() == title.lower():
+            return node.game
+
+        if title.lower() < node.game.title.lower():
+            return self._search(node.left, title)
+
+        return self._search(node.right, title)
