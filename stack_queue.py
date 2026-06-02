a96ec44824b36b2852073e1470f06735a98643ab
naila zahrah ramadhani <nailazahrah44@gmail.com>
2026-06-02 09:18:07 +0700

    Implementasi Stack dan Queue

---
+++ stack_queue.py
@@ -0,0 +1,25 @@
+class Stack:
+  def __init__(self):
+    self.daftar_item = []
+    
+  def push(self, item):
+    self.daftar_item.append(item)
+
+  def pop (self):
+    if self.daftar_item.pop()
+
+  def display(self):
+    return self.daftar_item
+
+class Queue:
+  def __init__(self):
+    self.daftar_item = []
+
+  def enqueue(self, item):
+    self.daftar_item.append(item)
+
+  def dequeue(self):
+    if self.daftar_item.pop(0)
+
+  def display(self):
+    return self.daftar_item
